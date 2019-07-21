c45ce7368a4c3a5141d0577265656c74ec77804d
ssimbul <spencer44simbul@gmail.com>
2019-07-20 22:42:21 -0400

    same changes , staging did not complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,4 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  first = "Spencer ";
-  last = "Simbul";
-  profile = "../assets/img/profile-removebg-preview.png";
-  background = "..assets/img/background.jpg";
-}
+export class AppComponent {}
