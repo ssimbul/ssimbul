6fdf73aa28f08bdc5d31e14b1e7bd5d0378727d5
ssimbul <spencer44simbul@gmail.com>
2019-07-20 23:05:29 -0400

    all routing paths added, scss re organised

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule, routingComponents } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MaterialModule } from "./material/material.module";
 import { HomeComponent } from "./components/home/home.component";
+import { EducationComponent } from './components/education/education.component';
+import { ExpComponent } from './components/exp/exp.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, routingComponents],
+  declarations: [AppComponent, routingComponents, EducationComponent, ExpComponent, ProjectsComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
